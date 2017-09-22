db4b54dea2c63f2f2e99ed8c354e166021da7401
Anders <idris.maps@gmx.ch>
2017-09-22 09:07:19 +0200

    Typo

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res, next) => res.send({ msg: 'hello' }))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Listening on port ${env.port}`)
+  console.log(`Listening on port ${port}`)
 })
