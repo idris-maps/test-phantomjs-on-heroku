44f7d2e34375e11d7a7f64bd9c3d30c3010b0dbb
Anders <idris.maps@gmx.ch>
2017-09-22 09:37:31 +0200

    Enable cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const pdf = require('html-pdf')
-const cors = require('cors'
-)
+const cors = require('cors')
+
 const app = express()
 app.use(require('body-parser').json())
 app.use(cors())
