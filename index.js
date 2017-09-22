435a6a66b75d60abcfa2eb844b7cce85e63ba946
Anders <idris.maps@gmx.ch>
2017-09-22 09:06:10 +0200

    Setup express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const pdf = require('html-pdf')
+
+const app = express()
+app.use(require('body-parser').json())
+
+app.get('/', (req, res, next) => res.send({ msg: 'hello' }))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${env.port}`)
+})
