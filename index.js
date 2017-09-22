0a47d1912d2e29dc881cc4750e51b296551db613
Anders <idris.maps@gmx.ch>
2017-09-22 09:29:43 +0200

    Create pdf

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.use(require('body-parser').json())
 
 app.get('/', (req, res, next) => res.send({ msg: 'hello' }))
 
+const verifyHasHtml = (req, res, next) => {
+  if (req.body.html) { return next() }
+  return res.status(400).json({ msg: 'html?' })
+}
+app.post('/pdf', [
+  verifyHasHtml,
+], (req, res) => {
+  pdf.create(req.body.html).toBuffer((err, buffer) => {
+    if (err) {
+      return res.status(400).json({ msg: 'Did not work' })
+    }
+    return res.status(200).json({ buffer })
+  })
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
